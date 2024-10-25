433caf23aa21be9635f6a5ec195961f63fb82eef
leonardo dias <leonardodias@ucl.br>
2024-10-25 14:11:34 -0300

    copy nvim configs

---
+++ nvim/lua/plugins/matchtag.lua
@@ -0,0 +1,4 @@
+return {
+  'gregsexton/matchtag',
+
+}
---
+++ nvim/lua/plugins/ts-autotag.lua
@@ -0,0 +1,10 @@
+return {
+  'windwp/nvim-ts-autotag',
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    opts = {}, -- this is equalent to setup({}) function
+  },
+  'gregsexton/matchtag',
+}
+
