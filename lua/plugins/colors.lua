cfd5035e9f4ed8e6e8a3b3f34d66d78203c19826
leonardo dias <leonardodias@ucl.br>
2025-08-15 17:56:59 -0300

    comment gruvbox color

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -11,6 +11,7 @@ return {
             -- Like many other themes, this one has different styles, and you could load
             -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
             vim.cmd.colorscheme('kanagawa-paper')
+            -- vim.cmd.colorscheme('gruvbox')
 
             -- You can configure highlights by doing something like:
             -- vim.cmd.hi 'Comment gui=none'
